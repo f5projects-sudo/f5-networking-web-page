8422193863af5e7a091893ff00aea511a9ec94b6
F5 Networking Assistant <ai-assistant@f5networking.com>
2026-03-19 12:56:33 -0600

    Correct footer map address to Miguel Blanco 1440

--- src/components/MapFooter.jsx
+++ src/components/MapFooter.jsx
@@ -4,7 +4,7 @@ export default function MapFooter() {
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative', overflow: 'hidden', borderRadius: '20px' }}>
       <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.938374856986!2d-103.358255!3d20.672085999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b1fdf339b085%3A0xaf19b832e58686ca!2sC.%20Miguel%20Blanco%201449%2C%20Col%20Americana%20Americana%2C%2044160%20Guadalajara%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1773946042052!5m2!1ses-419!2smx" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.936577322971!2d-103.36079602390176!3d20.67229859992523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b1fd8cfb27dd%3A0xa9d349e3edc5505b!2sC.%20Miguel%20Blanco%201440%2C%20Col%20Americana%2C%20Americana%2C%2044160%20Guadalajara%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1742410515152!5m2!1ses-419!2smx" 
         width="100%" 
         height="100%" 
         style={{ border: 0, filter: 'invert(90%) hue-rotate(180deg) brightness(0.9) contrast(1.1)' }} 
